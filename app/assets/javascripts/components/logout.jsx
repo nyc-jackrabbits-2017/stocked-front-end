150ecba74c14c35753fc944af685c8a40350fab3
Natalie Liles <Natalieliles@gmail.com>
2017-01-09 17:28:01 -0500

    Modify form and components.

---
+++ app/assets/javascripts/components/logout.jsx
@@ -0,0 +1,21 @@
+class Logout extends React.Component {
+  handleSubmit(event) {
+    event.preventDefault();
+    }
+
+    $.ajax({
+       url: 'sessions/:id',
+       method: 'delete',
+       data: {
+         authenticity_token: Functions.getMetaContent("csrf-token")
+       }
+    }).done(function() {
+
+    });
+
+  render() {
+    return (
+         <a href="#" onClick={this._signOut}>Sign out</a>
+       )
+    }
+}
