2174010143f6c8f8ff2d335171ac2bc04bf37b65
Natalie_Liles <Natalieliles@gmail.com>
2017-01-09 21:28:40 -0500

    Create new form template. CSS not working on templates.

--- app/assets/javascripts/components/login.jsx
+++ app/assets/javascripts/components/login.jsx
@@ -23,18 +23,45 @@ class Login extends React.Component {
 
 render() {
   return(
-    <form onSubmit={this.handleSubmit.bind(this)}>
-      <div className="form-group">
-        <label htmlFor="email">Email address:</label>
-        <input type="email" className="form-control" id="email" ref="email"/>
-      </div>
-      <div className="form-group">
-        <label htmlFor="pwd">Password:</label>
-        <input type="password" className="form-control" id="pwd" ref="password"/>
-      </div>
-      <button type="submit" className="btn btn-default">Login</button>
-   </form>
+  <div class="container">
+  <form onSubmit={this.handleSubmit} class="well form-horizontal" action=" " method="post"  id="contact_form">
+    <fieldset>
 
+    <legend>Login</legend>
+    <br/>
+
+      <div class="form-group">
+        <label class="col-md-4 control-label">E-Mail</label>  
+          <div class="col-md-4 inputGroupContainer">
+          <div class="input-group">
+              <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+             <input name="email" placeholder="E-Mail Address" class="form-control"  type="email" ref="email"/>
+          </div>
+        </div>
+      </div>
+      <br/>
+         
+      <div class="form-group">
+        <label class="col-md-4 control-label">Password</label>  
+          <div class="col-md-4 inputGroupContainer">
+          <div class="input-group">
+              <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+              <input name="password" placeholder="Password" class="form-control"  type="password" ref="password"/>
+          </div>
+      </div>
+      </div>
+      <br/>
+
+      <div class="form-group">
+        <label class="col-md-4 control-label"></label>
+        <div class="col-md-4">
+          <button type="submit" class="btn btn-warning" >Login <span class="glyphicon glyphicon-log-in"></span></button>
+        </div>
+      </div>
+
+    </fieldset>
+  </form>
+  </div>
    )
   }
 }
--- app/assets/javascripts/components/register.jsx
+++ app/assets/javascripts/components/register.jsx
@@ -20,17 +20,43 @@ class Register extends React.Component {
 
 render() {
   return(
-    <form onSubmit={this.handleSubmit}>
-      <div className="form-group">
-        <label htmlFor="email">Email address:</label>
-        <input type="email" className="form-control" id="email" ref="email"/>
+  <form onSubmit={this.handleSubmit} class="well form-horizontal" action=" " method="post"  id="contact_form">
+    <fieldset>
+
+    <legend>Create Account</legend>
+    <br/>
+
+      <div class="form-group">
+        <label class="col-md-4 control-label">E-Mail</label>  
+          <div class="col-md-4 inputGroupContainer">
+          <div class="input-group">
+              <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+             <input name="email" placeholder="E-Mail Address" class="form-control"  type="email" ref="email"/>
+          </div>
+        </div>
       </div>
-      <div className="form-group">
-        <label htmlFor="pwd">Password:</label>
-        <input type="password" className="form-control" id="pwd" ref="password"/>
+      <br/>
+         
+      <div class="form-group">
+        <label class="col-md-4 control-label">Password</label>  
+          <div class="col-md-4 inputGroupContainer">
+          <div class="input-group">
+              <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+              <input name="password" placeholder="Password" class="form-control"  type="password" ref="password"/>
+          </div>
       </div>
-      <button type="submit" className="btn btn-default">Create Account</button>
-   </form>
+      </div>
+      <br/>
+
+      <div class="form-group">
+        <label class="col-md-4 control-label"></label>
+        <div class="col-md-4">
+          <button type="submit" class="btn btn-warning" >Create Account <span class="glyphicon glyphicon-log-in"></span></button>
+        </div>
+      </div>
+
+    </fieldset>
+  </form>
    )
   }
 }
