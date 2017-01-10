05f200a30d947f5da4dde32a769d68e75fec800d
Alex Giacobbe <agiacobbe18@gmail.com>
2017-01-09 20:54:31 -0500

    chang PieCharts component to Charts component

--- app/assets/javascripts/components/charts.jsx
+++ app/assets/javascripts/components/charts.jsx
@@ -1,17 +1,17 @@
-class PieCharts extends React.Component {
+class Charts extends React.Component {
   render(){
     return(
       <div>
         <h1 className="page-header">Dashboard</h1>
         <div className="row placeholders">
           <div className="col-xs-6 col-sm-3 placeholder">
-            <Pie name="Portfolio-Performance" />
-            <h4>Label</h4>
+            <Pie name="Portfolio1" />
+            <h4>Portfolio Shares</h4>
             <span className="text-muted">Something else</span>
           </div>
           <div className="col-xs-6 col-sm-3 placeholder">
             <Pie name="Portfolio2"/>
-            <h4>Label</h4>
+            <h4>Positions Price</h4>
             <span className="text-muted">Something else</span>
           </div>
           <div className="col-xs-6 col-sm-3 placeholder">
