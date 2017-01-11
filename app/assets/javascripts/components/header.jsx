04090e02da8ca5aae82034e15577ff0abbcbb75e
Emily Gottlieb <email": null>
2017-01-11 13:25:44 -0500

    header navbar works

--- app/assets/javascripts/components/header.jsx
+++ app/assets/javascripts/components/header.jsx
@@ -1,5 +1,28 @@
 class Header extends React.Component {
+
+  handleClick(e){
+    e.preventDefault()
+
+    $.ajax({
+      url: "http://stocked-back.herokuapp.com/api/sessions/"+ window.localStorage.token,
+      type: "DELETE"
+    }).done(function(response){
+      location.href = "/sessions/new"
+    })
+  }
+
   render() {
+
+    const logInNav = window.localStorage.user_id === '' ?
+      <ul className="nav navbar-nav navbar-right">
+        <li><a href="/users/new">Register</a></li>
+        <li><a href="/sessions/new">Login</a></li>
+      </ul> :
+      <ul className="nav navbar-nav navbar-right">
+        <li className="active"><a href="/">Overview <span className="sr-only">(current)</span></a></li>
+        <li><a href="" className="logout" onClick={this.handleClick.bind(this)}>Logout</a></li>
+      </ul>
+
     return(
       <nav className="navbar navbar-inverse navbar-fixed-top">
         <div className="container-fluid">
@@ -7,12 +30,7 @@ class Header extends React.Component {
             <a className="navbar-brand" href="/">Stocked</a>
           </div>
           <div id="navbar" className="navbar-collapse collapse">
-            <ul className="nav navbar-nav navbar-right">
-              <li><a href="#">Dashboard</a></li>
-              <li><a href="#">Settings</a></li>
-              <li><a href="#">Profile</a></li>
-              <li><a href="#">Help</a></li>
-            </ul>
+            {logInNav}
             <form className="navbar-form navbar-right">
               <input type="text" className="form-control" placeholder="Search..."/>
             </form>
--- app/assets/javascripts/components/nav_bar.jsx
+++ app/assets/javascripts/components/nav_bar.jsx
@@ -5,20 +5,28 @@ class NavBar extends React.Component {
 
     $.ajax({
       url: "http://stocked-back.herokuapp.com/api/sessions/"+ window.localStorage.token,
-      method: "DELETE"
+      type: "DELETE"
     }).done(function(response){
       location.href = "/sessions/new"
     })
   }
+
   render() {
+
+    const logInNav = window.localStorage.user_id === '' ?
+      <div>
+        <li><a href="/users/new">Register</a></li>
+        <li><a href="/sessions/new">Login</a></li>
+      </div> :
+      <div>
+        <li className="active"><a href="/">Overview <span className="sr-only">(current)</span></a></li>
+        <li><a href="" className="logout" onClick={this.handleClick.bind(this)}>Logout</a></li>
+      </div>
+
     return(
       <div className="col-sm-3 col-md-2 sidebar">
         <ul className="nav nav-sidebar">
-          <li className="active"><a href="/">Overview <span className="sr-only">(current)</span></a></li>
-          <li><a href="/">Portfolio</a></li>
-          <li><a href="/sessions/new">Register</a></li>
-          <li><a href="#">Login</a></li>
-          <li><a href="" className="logout" onClick={this.handleClick.bind(this)}>Logout</a></li>
+        {logInNav}
         </ul>
       </div>
     )
